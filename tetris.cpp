2d91389206adcecb72218839b8a4b6b0af6d37d7
yosyam <yoshihisa.y@gmail.com>
2013-04-14 02:19:43 +0900

    draw a block

--- tetris.cpp
+++ tetris.cpp
@@ -1,13 +1,51 @@
 #include <windows.h>
 
+#define STAGE_X 10
+#define STAGE_Y 20
+
 HINSTANCE hInstance;
 HWND hMainWindow;
 
+HDC hMemDC, hBlockDC;
+HBITMAP hMemPrev, hBlockPrev;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
   switch(msg) {
-  case WM_DESTROY:
+  case WM_CREATE: {
+    HDC hdc = GetDC(hWnd);
+
+    hMemDC = CreateCompatibleDC(hdc);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdc, 24 * 10, 24 * 20);
+    hMemPrev = (HBITMAP)SelectObject(hMemDC, hBitmap);
+
+    hBlockDC = CreateCompatibleDC(hdc);
+    hBitmap = LoadBitmap(hInstance, "BLOCKS");
+    hBlockPrev = (HBITMAP)SelectObject(hBlockDC, hBitmap);
+
+    // debug
+    BitBlt(hMemDC, 0, 0, 24, 24, hBlockDC, 0, 0, SRCCOPY);
+
+    ReleaseDC (hWnd, hdc);
+    break;
+  }
+  case WM_PAINT: {
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+    BitBlt(hdc, 0, 0, 24*10, 24*20, hMemDC, 0, 0, SRCCOPY);
+    EndPaint(hWnd, &ps);
+    break;
+  }
+  case WM_DESTROY: {
+    HBITMAP hBitmap = (HBITMAP)SelectObject(hMemDC, hMemPrev);
+    DeleteObject(hBitmap);
+    DeleteObject(hMemDC);
+    
+    hBitmap = (HBITMAP)SelectObject(hBlockDC, hBlockPrev);
+    DeleteObject(hBitmap);
+    DeleteObject(hBlockDC);
     PostQuitMessage(0);
     break;
+  }
   default:
     return DefWindowProc(hWnd, msg, wParam, lParam);
   }
@@ -41,10 +79,9 @@ int WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int cmdShow) {
 
   RECT r;
   r.left = r.top = 0;
-  r.right = 24 * 10;
-  r.bottom = 24 * 20;
+  r.right = 24 * STAGE_X;
+  r.bottom = 24 * STAGE_Y;
   AdjustWindowRectEx(&r, WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU | WS_CAPTION, false, 0);
-
   
   hMainWindow = CreateWindow(pClassName, "WinTetris", WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU | WS_CAPTION,
                              CW_USEDEFAULT, CW_USEDEFAULT,
